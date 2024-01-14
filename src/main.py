3b69217d85773aa01e4e1d99a48c88ec25f9c565
snimu <sebastian.nicolas.mueller@gmail.com>
2024-01-14 15:45:48 +0100

    hopefully fix printing this time...

--- src/main.py
+++ src/main.py
@@ -496,7 +496,7 @@ def eval(net):
 
 def train(num_steps, attn_type, **kwargs):
     # Initializing variables for the whole run.
-    total_time_seconds = 0.
+    time_secs = 0.
     microbatch_step = current_steps = 0
     microbatches_since_last_eval = 0. # TODO: Good way to simplify this?
     tokens_seen = 0
@@ -563,7 +563,7 @@ def train(num_steps, attn_type, **kwargs):
         if current_steps % 10 == 0 and microbatch_step % current_accumulate_steps == 0 and not current_steps % hyp['opt']['eval_iter'] == 0:
             train_acc = (outputs.detach().argmax(-1) == targets).float().mean().item()
             train_loss = loss.detach().cpu().item()
-            train_summary_variables = {'epoch': tokens_seen//len(data['train']), 'crnt_steps': current_steps, 'train_loss': train_loss, 'train_acc': train_acc, 'grad_norm': grad_norm, 'time_secs': total_time_seconds}
+            train_summary_variables = {'epoch': tokens_seen//len(data['train']), 'crnt_steps': current_steps, 'train_loss': train_loss, 'train_acc': train_acc}
             print_training_details(list(map(partial(format_for_table, locals=train_summary_variables), logging_columns_list)))
 
         loss.div(current_accumulate_steps).backward()
@@ -609,7 +609,7 @@ def train(num_steps, attn_type, **kwargs):
             if current_steps % hyp['opt']['eval_iter'] == 0:
                 ender.record()
                 torch.cuda.synchronize()
-                total_time_seconds += 1e-3 * starter.elapsed_time(ender)
+                time_secs += 1e-3 * starter.elapsed_time(ender)
                 train_loss = loss.detach().cpu().item() # To have an updated loss to compare with the eval loss
 
                 opt.zero_grad(set_to_none=True)
