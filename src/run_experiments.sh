8646bb2201ab3f0388bfd4c455f4ae9b2c0bc9be
snimu <sebastian.nicolas.mueller@gmail.com>
2024-03-19 08:55:36 +0100

    Remove hydra runs
    idc much, they cost time/money, can be done later if I want to

--- src/run_experiments.sh
+++ src/run_experiments.sh
@@ -2,17 +2,16 @@
 #   1. 100 tries (to get really stable data)
 #   2. approx 1 epoch (so that it doesnt take forever)
 # The experiments:
-#   1. Vanilla and Hydra attention each
-#   2. Test every combination of x_norm and qkv_norm
-#   3. Test every combination of x_norm and qk_norm (don't repeat tests without qk_norm)
+#   1. Test every combination of x_norm and qkv_norm
+#   2. Test every combination of x_norm and qk_norm (don't repeat tests without qk_norm)
 python main_llm.py -s --num_tries 100 --num_steps 1000 --seed 1000 \\
---attn_type vanilla hydra --use_x_norm 0 1 --use_qkv_norm 0 1
+--attn_type vanilla --use_x_norm 0 1 --use_qkv_norm 0 1
 
 python main_llm.py -s --append --num_tries 100 --num_steps 1000 --seed 1000 \\
---attn_type vanilla hydra --use_x_norm 0 --use_qk_norm 1
+--attn_type vanilla --use_x_norm 0 --use_qk_norm 1
 
 python main_llm.py -s --append --num_tries 100 --num_steps 1000 --seed 1000 \\
---attn_type vanilla hydra --use_x_norm 1 --use_qk_norm 1
+--attn_type vanilla --use_x_norm 1 --use_qk_norm 1
 
 mv ../results/results_llm.csv ../results/results_llm_1000_steps_100_tries.csv
 
@@ -21,12 +20,12 @@ mv ../results/results_llm.csv ../results/results_llm_1000_steps_100_tries.csv
 #   2. For 10 tries (10xepochs, 0.1xtries --> same time needs (approximately))
 #   3. Different seed (to avoid duplication)
 python main_llm.py -s --num_tries 10 --num_steps 10000 --num_epochs 10 --seed 2000 \\
---attn_type vanilla hydra --use_x_norm 0 1 --use_qkv_norm 0 1
+--attn_type vanilla --use_x_norm 0 1 --use_qkv_norm 0 1
 
 python main_llm.py -s --append --num_tries 10 --num_steps 10000 --num_epochs 10 --seed 2000 \\
---attn_type vanilla hydra --use_x_norm 0 --use_qk_norm 1
+--attn_type vanilla --use_x_norm 0 --use_qk_norm 1
 
 python main_llm.py -s --append --num_tries 10 --num_steps 10000 --num_epochs 10 --seed 2000 \\
---attn_type vanilla hydra --use_x_norm 1 --use_qk_norm 1
+--attn_type vanilla --use_x_norm 1 --use_qk_norm 1
 
 mv ../results/results_llm.csv ../results/results_llm_10_epochs_10_tries.csv
