d5f8688d397c3b3c6862590a976418b27cee34fb
snimu <sebastian.nicolas.mueller@gmail.com>
2024-03-31 15:01:21 +0200

    Set --num_epochs_train and --num_epochs_val

--- src/run_experiments_v040_h100.sh
+++ src/run_experiments_v040_h100.sh
@@ -1,5 +1,6 @@
 # run on H100 -> don't double token capacity, just enjoy the extra speed!!!
 # Without any norm, the grad_norm will be nan -> always use some norm
 # Different seed than on A10 to avoid redundancy
-python main_hlb_v040.py -s --num_runs 10 --seed 2000 --use_x_norm 1 --use_qk_norm 0 1 --model_scale 0.5 1.0 5.0 10.0 --savefile "results_v040_1000_steps_10_tries_sqrt_dh.csv"
-python main_hlb_v040.py -s --append --num_runs 10 --seed 2000 --use_x_norm 0 --use_qk_norm 1 --model_scale 0.5 1.0 5.0 10.0 --savefile "results_v040_1000_steps_10_tries_sqrt_dh.csv"
+# More train than val epochs in hopes that run ends in validation; but if jumps in epochs are too high, don't waste too much time either
+python main_hlb_v040.py -s --num_runs 10 --seed 2000 --use_x_norm 1 --use_qk_norm 0 1 --model_scale 0.5 1.0 5.0 10.0 --num_epochs_train 3 --num_epochs_val 2 --savefile "results_v040_1000_steps_10_tries_sqrt_dh.csv"
+python main_hlb_v040.py -s --append --num_runs 10 --seed 2000 --use_x_norm 0 --use_qk_norm 1 --model_scale 0.5 1.0 5.0 10.0 --num_epochs_train 3 --num_epochs_val 2 --savefile "results_v040_1000_steps_10_tries_sqrt_dh.csv"
