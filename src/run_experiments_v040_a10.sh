92292cb0e836ca73a293122e10d28893f3706a19
snimu <sebastian.nicolas.mueller@gmail.com>
2024-03-31 14:53:35 +0200

    Test linear vs non-linear

--- src/run_experiments_v040_a10.sh
+++ src/run_experiments_v040_a10.sh
@@ -1,4 +1,4 @@
 # run on A10 -> half token capacity
 # Without any norm, the grad_norm will be nan -> always use some norm
-python main_hlb_v040.py -s --num_runs 10 --seed 1000 --use_x_norm 1 --use_qk_norm 0 1 --model_scale 0.5 1.0 --token_capacity_factor 0.4 --savefile "results_v040_preliminary_1000_steps_10_tries_sqrt_dh.csv"
-python main_hlb_v040.py -s --append --num_runs 10 --seed 1000 --use_x_norm 0 --use_qk_norm 1 --model_scale 0.5 1.0 --token_capacity_factor 0.4 --savefile "results_v040_preliminary_1000_steps_10_tries_sqrt_dh.csv"
+python main_hlb_v040.py -s --num_runs 10 --seed 1000 --linear 0 1 --use_x_norm 1 --use_qk_norm 0 1 --model_scale 0.5 1.0 --token_capacity_factor 0.4 --savefile "results_v040_preliminary_1000_steps_10_tries_sqrt_dh.csv"
+python main_hlb_v040.py -s --append --num_runs 10 --seed 1000 --linear 0 1 --use_x_norm 0 --use_qk_norm 1 --model_scale 0.5 1.0 --token_capacity_factor 0.4 --savefile "results_v040_preliminary_1000_steps_10_tries_sqrt_dh.csv"
